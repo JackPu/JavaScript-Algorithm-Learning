09d43be36d37afee8e72571408997c53c7a4cdef
JackPu <kakashjack@gmail.com>
2016-09-28 08:33:06 +0800

    add new test

--- karma.conf.js
+++ karma.conf.js
@@ -54,11 +54,9 @@ module.exports = function(config) {
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
+    browsers: ['Firefox'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
