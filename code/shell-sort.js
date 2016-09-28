cf6a1a0002c8dbcfcfe5127deebc32ba543afc7a
JackPu <kakashjack@gmail.com>
2016-09-28 21:27:52 +0800

    add new js

---
+++ code/shell-sort.js
@@ -0,0 +1,28 @@
+/**
+** shell sort O(Nlog2N)
+**/
+
+function shellSort(arr) {
+    let l = arr.length;
+    let gap = l >> 1;
+    
+    while(gap>0) {
+        for(let i = gap;i<l;i++) {
+            let tem = arr[i];
+            let j = i - gap; 
+            for(;j>=0&&tem<arr[j];j-=gap){
+                arr[j+gap] = arr[j];
+            }
+            arr[j+gap] = tem;
+        }
+        gap = gap >> 1;
+    }
+    
+    
+    
+    return arr;
+    
+}
+
+module.exports = shellSort;
+
--- tests/shell-sort.test.js
+++ tests/shell-sort.test.js
@@ -1,5 +1,5 @@
-var inserSort = require('../code/insert-sort.js');
-it('Insert Sort', function () {
+var inserSort = require('../code/shell-sort.js');
+it('Shell Sort', function () {
     let inputArr = [1,111,2,12,180,500,-12,0];
     let outputArr = inserSort(inputArr);
     expect(outputArr).toEqual([-12,0,1,2 ,12,111,180,500]);
