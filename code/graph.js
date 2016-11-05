29e10a097dcfd334ab34919fb2754e83c48abe8f
JackPu <kakashjack@gmail.com>
2016-11-05 19:39:38 +0800

    add new js

--- code/graph.js
+++ code/graph.js
@@ -14,11 +14,63 @@ class Graph(v) {
     this.vertices = v;
     this.edges = 0;
     this.adj = [];
+    this.marked = [];
+    for(let i=0;i<v;++i) {
+      this.adj[i] = [];
+      this.adj[i].push('');
+      this.marked[i] = false;
+    }
+    
     
   }
   
-  addEdge() {
+  addEdge(v, w) {
+    this.adj[v].push(w);
+    this.adj[w].push(v);
+    this.edges++;
+  }
   
+  showGrap() {
+    for(let i=0;i<this.vertices;i++) {
+      console.log(i + '->');
+      for(let j=0;j<this.vertices;++j) {
+        if(this.adj[i][j] != undefined) {
+          console.log(this.adj[i][j] + ' ');
+        }
+      }
+    }
+  }
+  
+  dfs(v) {
+    this.marked[v] = true;
+    if(this.adj[v] != undefined) {
+      for(let j in this.adj[v]) {
+        if(!this.marked[j]) {
+          this.dfs(w);  
+        }  
+      }
+    }
+  }
+  
+  bfs(s) {
+    let q = [];
+    this.marked[s] = true;
+    q.push(s);
+    while(q.length>0) {
+      let v = q.shift();
+      if(this.adj[v]!= undefined) {
+        //
+      }
+      for(let j in this.adj[v]) {
+        if(!this.marked[j]) {
+          this.marked[j] = true;
+          q.push(j);
+        }
+        
+      }
+    }
   }
   
 }
+
+
